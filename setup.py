c3ce899460a9aca572f27dab9bbcc076479d8801
p513817 <p513817@gmail.com>
2023-07-10 17:21:57 +0800

    feat: update to 0.0.2 and fix the openc version larger than 4

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='cicv',
-    version='0.0.1',
+    version='0.0.2',
     author='Max Chang',
     author_email='p513817@gmail.com',
     description='A common interface for opencv',
     packages=['cicv'],
     python_requires='>=3.6',
     install_requires=[
-        "opencv-python"
+        "opencv-python>=4"
     ],
 )
